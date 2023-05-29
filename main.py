552636145e2a6438b842bb893d9987faa129481d
jtimko <justin@jtimko.com>
2023-05-28 19:16:18 -0700

    Switched to Classes

--- main.py
+++ main.py
@@ -2,23 +2,34 @@
 
 import pygame
 
-pygame.init()
-x = 1080/2
-y = 1920/2
+class PictureFrame:
 
-screen = pygame.display.set_mode((y, x))
-pygame.display.set_caption("Picture Frame!")
+    def __init__(self):
+        self.x = 1080/2
+        self.y = 1920/2
+        self.screen = pygame.display.set_mode((self.y, self.x))
 
-img = pygame.image.load("img/me.jpg")
-img = pygame.transform.scale(img, (x, y))
-img = pygame.transform.rotate(img, 90)
+    def load_image(self, image):
+        self.img = pygame.image.load(image)
+        self.img = pygame.transform.scale(self.img, (self.x, self.y))
+        self.img = pygame.transform.rotate(self.img, 90)
 
-screen.blit(img, (0, 0))
+    def display_image(self):
+        self.screen.blit(self.img, (0, 0))
+        pygame.display.flip()
 
-pygame.display.flip()
+picture = PictureFrame()
+picture.load_image("img/me.jpg")
+picture.display_image()
 
 while True:
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                picture.load_image("img/you.png")
+                picture.display_image()
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+
+
