7a583f62b0994f1a3e1e86c65bb6f26006b05032
jtimko <justin@jtimko.com>
2023-05-29 15:58:25 -0700

    Fixed Errors with Server

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request
 import os
+import requests
 
 DOWNLOAD_DIRECTORY = os.getcwd() + '/img/'
 app = Flask(__name__)
 
-app.route('/sms', methods=['POST'])
+@app.route('/sms', methods=['POST'])
 def sms():
     resp = MessagingResponse()
     if request.values['NumMedia'] != 0:
         filename = request.values['MessageSid'] + '.jpg'
         with open("{}/{}".format(DOWNLOAD_DIRECTORY, filename), 'wb') as f:
             image_url = request.values['MediaUrl0']
-            f.write(request.get(image_url).content)
+            f.write(requests.get(image_url).content)
 
         resp.message("Thanks for the image!")
     else:
