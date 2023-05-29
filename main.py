04e9d7f02362b11f60eb8943954e246860bffcb2
jtimko <justin@jtimko.com>
2023-05-28 21:58:09 -0700

    Cleaned code and easy picture update

--- main.py
+++ main.py
@@ -2,34 +2,18 @@
 
 import pygame
 import os
-from twilio.rest import Client
-
-
-# Testing twilio. Will involve listening for a mms message and then displaying the image on the screen.
-
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    from_='' + os.environ['TWILIO_FROM'],
-    body='Hello there!',
-    to='' + os.environ['TWILIO_TO']
-)
-
-print(message.sid)
 
 # Created a picture frame class that will display an image on the screen. Pygame is used to provide the window.
-
 class PictureFrame:
-
     def __init__(self):
+        self.path = os.getcwd() + '/img/'
         self.x = 1080/2
         self.y = 1920/2
         self.screen = pygame.display.set_mode((self.y, self.x))
+        self.current = sorted(filter(os.path.isfile, map(self.get_latest_file, os.listdir(self.path))), key=os.path.getctime, reverse=True)
 
-    def load_image(self, image):
-        self.img = pygame.image.load(image)
+    def load_image(self):
+        self.img = pygame.image.load(self.current[0])
         self.img = pygame.transform.scale(self.img, (self.x, self.y))
         self.img = pygame.transform.rotate(self.img, 90)
 
@@ -37,17 +21,28 @@ class PictureFrame:
         self.screen.blit(self.img, (0, 0))
         pygame.display.flip()
 
+    def check_for_updates(self):
+        self.check = sorted(filter(os.path.isfile, map(self.get_latest_file, os.listdir(self.path))), key=os.path.getctime, reverse=True)
+
+    def get_latest_file(self, item):
+        if not item.startswith('.'):
+            return self.path + item
+        else:
+            return ''
+
 # The execution of the program starts here.
 picture = PictureFrame()
-picture.load_image("img/me.jpg")
+picture.load_image()
 picture.display_image()
 
 while True:
+    picture.check_for_updates()
+    
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                picture.load_image("img/you.jpg")
-                picture.display_image()
+        if picture.check[0] != picture.current[0]:
+            picture.current = picture.check
+            picture.load_image()
+            picture.display_image()
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
