52269faf154172e5dfa61aab5dc4e8e86355d257
jtimko <justin@jtimko.com>
2023-05-28 20:13:58 -0700

    Adding Twilio

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 import pygame
+import os
+from twilio.rest import Client
+
+
+# Testing twilio. Will involve listening for a mms message and then displaying the image on the screen.
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_='' + os.environ['TWILIO_FROM'],
+    body='Hello there!',
+    to='' + os.environ['TWILIO_TO']
+)
+
+print(message.sid)
+
+# Created a picture frame class that will display an image on the screen. Pygame is used to provide the window.
 
 class PictureFrame:
 
@@ -18,6 +37,7 @@ class PictureFrame:
         self.screen.blit(self.img, (0, 0))
         pygame.display.flip()
 
+# The execution of the program starts here.
 picture = PictureFrame()
 picture.load_image("img/me.jpg")
 picture.display_image()
@@ -26,7 +46,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                picture.load_image("img/you.png")
+                picture.load_image("img/you.jpg")
                 picture.display_image()
         if event.type == pygame.QUIT:
             pygame.quit()
